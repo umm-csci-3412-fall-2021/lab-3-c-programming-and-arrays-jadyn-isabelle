e888361bd536b75881371c2649972a81780a9052
Jadyn Sondrol <sondr037@morris.umn.edu>
2021-10-05 10:30:33 -0500

    Finish array_merge method
    Co-Authored-By: IsabelleHjelden <hjeld013@morris.umn.edu>

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,35 +1,63 @@
 #include "array_merge.h"
-#include <../mergesort/mergesort.c>
+#include "../mergesort/mergesort.c"
+#include "../mergesort/mergesort.h"
+#include <stdlib.h>
+#include <cstdio>
+#include <cstring>
 
+// num_arrays is the number of arrays
+// sizes is an array of the size of each corresponding array
+// values is an array with the values of each array
 int* array_merge(int num_arrays, int* sizes, int** values) {
   
-  int totalSize=0;
-  int index =1;
+  int totalSize=0; // the total size of the end array
+  int index =0;
 
   for(int i=0; i < num_arrays; ++i){
-	  totalSize = totalSize + sizes[i];
+	  totalSize = totalSize + sizes[i]; // adds the numbers of each array together
   }	  
-  totalSize += 1;
-  int* tempArray = (int*)malloc(sizeof(int)*totalSize); 
+  
+  int* tempArray = (int*) calloc(totalSize, sizeof(int)); // array of all the values in values
 
-  for(int i=0; i < num_arrays; ++i){
+  for(int i=0; i < num_arrays; ++i){ 
 	  for(int j=0; j < sizes[i]; ++j){
-		  tempArray[index]= values[i][j];
+		  tempArray[index]= values[i][j]; //adding the values of values into the array
 		  ++index;
 		 }
   }
-  // need to check if num_arrays = 0
-  mergesort(totalSize, tempArray);
 
-  int place = 2;
-  for(int i=2, i< totalSize; ++i)
-	  if (temp[i] != temp[i-1]){
-		temp[place]=temp[i];
-		++place;
+  mergesort(totalSize, tempArray); // call our mergesort function to sort the array
+
+  int number;
+
+  if (num_arrays != 0){
+ 	  number = 1;
+  } else {
+	  number =0;
+  }
+
+  for(int i=1; i < totalSize; i++){ 
+	if(tempArray[i] != tempArray[i-1]){ // checking if there are any duplicate numbers
+	  ++number;
+	}
+  }
+
+  index =2;
+  int* result= (int*) calloc(number+1, sizeof(int));
+
+  result[0]= number; //number of unique elements
+  
+  if(number != 0) {
+	  result[1] = tempArray[0];
+  }
+
+  for(int i=1; i< totalSize; ++i){
+	  if(tempArray[i] != tempArray[i-1]){ //if its not a duplicate number 
+		  result[index]= tempArray[i]; // add it to tempArray
+	          ++index;
 	  }
   }
 
-
-	  
-  return sizes;
+  free(tempArray);
+  return result;
 }
